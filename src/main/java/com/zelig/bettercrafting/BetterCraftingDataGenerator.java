cb2bea729821597c4e721aba99ab470cb8aa4fae
Seligmann <zachary.a.seligman@gmail.com>
2024-12-21 21:18:25 -0600

    lole

---
+++ src/main/java/com/zelig/bettercrafting/BetterCraftingDataGenerator.java
@@ -0,0 +1,11 @@
+package com.zelig.bettercrafting;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BetterCraftingDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
